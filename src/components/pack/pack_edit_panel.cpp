49bc43a657eb39e55a2a607670c99afdf6a35f03
ChengQi <cq-chengqi@outlook.com>
2024-11-14 17:48:41 +0800

    ImGui

--- src/components/pack/pack_edit_panel.cpp
+++
@@ -1,6 +0,0 @@
-#include "pack_edit_panel.h"
-
-PackEditPanel::PackEditPanel(wxWindow *parent)
-    : PackPanelUI(parent)
-{
-}
--- xmake.lua
+++ xmake.lua
@@ -1,122 +1,13 @@
-
 add_rules("mode.debug", "mode.release")
 
+add_requires("imgui", { configs ={ glfw_opengl3 = true, opengl3 = true }})
 
-add_requires("wxwidgets", { configs = { shared = true, runtimes = "MD", debug = true } })
-
-set_languages("c++20")
-
-target("my-tools")
+target("my_tools")
     set_kind("binary")
     add_files("src/**.cpp")
-    add_includedirs("src/themes/my", "src/components/pack", "src/components/ui","src/frames")
-    
-    add_packages("wxwidgets")
+    add_packages("imgui")
+    add_includedirs("src")
 
-    before_build(function (target)
-        local cpps = target:sourcefiles()
-        local command = "xgettext --from-code=UTF-8 --keyword=_ -o ./src/locale/zh_CN/LC_MESSAGES/message.pot -L C++ "
-     
-        -- 生成 pot 文件
-        for _, file in ipairs(cpps) do
-            command = command .. " " .. file .. " "
-
-            if not file:endswith("main.cpp") then
-                command = command .. " " .. string.gsub(file, "%.cpp$", ".h")
-            end
-        end
-        os.exec(command)
-
-        -- 生成 po 文件
-        command = "msgmerge --update ./src/locale/zh_CN/LC_MESSAGES/message.po ./src/locale/zh_CN/LC_MESSAGES/message.pot"
-        os.exec(command)
-
-        -- 生成 mo 文件
-        command = "msgfmt ./src/locale/zh_CN/LC_MESSAGES/message.po -o ./src/locale/zh_CN/LC_MESSAGES/message.mo"
-        os.exec(command)
+    after_build(function (target) 
+        os.cp("fonts", path.join(target:targetdir()))
     end)
-
-    after_build(function (target)
-        os.cp("src/locale", path.join(target:targetdir(), "locale"))
-    end)
-
-    on_clean(function (target) 
-        os.rm(path.join(target:targetdir(), "locale"))
-    end)
-
-if is_plat("windows") then
-    add_cxflags("/utf-8")
-end
-
-
-
-
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro definition
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
